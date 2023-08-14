e8af31ac952e1ade353a288c2ff96370a79b50ae
Jamshid CT <jamshid.ct@idcglobal.com>
2023-08-14 17:46:10 +0530

    initial commit

---
+++ src/main/java/com/gravityScale/gravityScale/GravityScaleApplication.java
@@ -0,0 +1,13 @@
+package com.gravityScale.gravityScale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GravityScaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GravityScaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gravityScale/gravityScale/GravityScaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gravityScale.gravityScale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GravityScaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
